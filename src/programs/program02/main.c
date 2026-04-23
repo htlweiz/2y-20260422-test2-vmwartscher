5ea49a469d02f6124747c7e83a9dc3da6c251dcd
Vali <valerie.wartscher@sr.htlweiz.at>
2026-04-23 09:27:21 +0000

    Hilfe

--- src/programs/program02/main.c
+++ src/programs/program02/main.c
@@ -8,7 +8,13 @@
 int main(int argc, char **argv) {
   int16_t count = 0;
   // write your solution here
-  
+   
+  do ( number < 6) {
+    printf("Gib eine Zahl ein:");
+  }while ( ! = 0) {
+
+    return number;
+  }
 
   printf("Du hast %d Mal gewürfelt.", count);
 }
--- src/programs/program04/main.c
+++ src/programs/program04/main.c
@@ -20,7 +20,8 @@ int main(void) {
   double leg_a = 0, leg_b = 0;
 
   scanf("%lf", &leg_a);
-
+  
+  printf("Die HYP ist:");
   printf("%f", compute_hypotenuse(leg_a, leg_b));
 
   return 0;
